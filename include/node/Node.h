3adc545f9c81691d46e929b417b9f4f97c34d966
wql <2294336102@qq.com>
2023-07-30 18:07:11 +0800

    jisuanqi

---
+++ include/node/Node.h
@@ -0,0 +1,102 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+class Node
+{
+private:
+public:
+    virtual ~Node() = 0;
+    virtual double Calc() const = 0;
+};
+class NumberNode : public Node
+{
+private:
+    const double number_;
+
+public:
+    NumberNode(double num) : number_(num){};
+    ~NumberNode(){};
+
+public:
+    double Calc() const;
+};
+// BinaryNode
+class BinaryNde : public Node
+{
+protected:
+    Node *const left_;
+    Node *const right_;
+
+public:
+    BinaryNde(Node *left, Node *right) : left_(left), right_(right){};
+    ~BinaryNde()
+    {
+        delete left_;
+        delete right_;
+    };
+};
+
+// UnaryNode
+class UnaryNode : public Node
+{
+protected:
+    Node *const child_;
+
+public:
+    UnaryNode(Node *child) : child_(child){};
+    ~UnaryNode()
+    {
+        delete child_;
+    };
+};
+
+class AddNode : public BinaryNde
+{
+private:
+    /* data */
+public:
+    AddNode(Node *left, Node *right) : BinaryNde(left, right){};
+    ~AddNode(){};
+    double Calc() const;
+};
+
+class SubNode : public BinaryNde
+{
+private:
+    /* data */
+public:
+    SubNode(Node *left, Node *right) : BinaryNde(left, right){};
+    ~SubNode(){};
+    double Calc() const;
+};
+
+class MultipleNode : public BinaryNde
+{
+private:
+    /* data */
+public:
+    MultipleNode(Node *left, Node *right) : BinaryNde(left, right){};
+    ~MultipleNode(){};
+    double Calc() const;
+};
+
+class DividedNode : public BinaryNde
+{
+private:
+    /* data */
+public:
+    DividedNode(Node *left, Node *right) : BinaryNde(left, right){};
+    ~DividedNode(){};
+    double Calc() const;
+};
+
+class UminusNode : public UnaryNode
+{
+private:
+    /* data */
+public:
+    UminusNode(Node *child) : UnaryNode(child){};
+    ~UminusNode(){};
+    double Calc() const;    
+};
+
+#endif // _NODE_H_
---
+++ lib/Node.cpp
@@ -0,0 +1,40 @@
+#include "../include/node/Node.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+double NumberNode::Calc() const
+{
+    return number_;
+}
+
+double AddNode::Calc() const
+{
+    return left_->Calc() + right_->Calc();
+}
+
+double SubNode::Calc() const
+{
+    return left_->Calc() - right_->Calc();
+}
+
+double MultipleNode::Calc() const
+{
+    return left_->Calc() * right_->Calc();
+}
+
+double DividedNode::Calc() const
+{
+    if (right_->Calc() != 0)
+        return left_->Calc() * right_->Calc();
+    else
+    {
+        cout << "Error the divied is Zro" << endl;
+        return HUGE_VAL;
+    }
+}
+
+double UminusNode::Calc() const
+{
+    return -child_->Calc();
+}
