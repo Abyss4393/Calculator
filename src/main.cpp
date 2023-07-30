c48e8fe9945f97ba541f0072847d9e65218aec2a
wql <2294336102@qq.com>
2023-07-30 21:10:46 +0800

    执行器

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    cout << ">>";
+    string buffer;
+    getline(cin, buffer);
+    cout << buffer << endl;
 }
