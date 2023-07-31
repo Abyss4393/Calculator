fd77f0b28b3ded71dca46ca73bade58fd25a9299
wql <2294336102@qq.com>
2023-07-31 09:47:55 +0800

    new scanner.h

--- include/tools/Scanner.h
+++ include/tools/Scanner.h
@@ -4,15 +4,36 @@ using namespace std;
 #ifndef _SCANNER_H_
 #define _SCANNER_H_
 
+enum EStatus
+{
+    END,
+    ERROR,
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLE,
+    DIVIDE,
+    LPARENTHESES,
+    RPARENTHESES,
+    IDENTIFIER,
+    ASSIGN
+};
+
 class Scanner
 {
 private:
     const string buf_;
+    unsigned int curPos_;
+    EStatus sus_;
+    double num_;
+
+    void SkipWhite();
+
 public:
-    Scanner(const string &buf): buf_(buf){};
-    ~Scanner(){};
+    Scanner(const string &buf);
+    ~Scanner();
+    void Accept();
+    double Number() const;
+    EStatus Status() const;
 };
-
-\
-
 #endif // _SCANNER_H_
