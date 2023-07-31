223c0bf3aafc9627e029e1af3b67723ad5abdb03
wql <2294336102@qq.com>
2023-07-31 08:29:31 +0800

    解析器源文件

---
+++ lib/toollib/Parser.cpp
@@ -0,0 +1,11 @@
+#include "../include/tools/Parser.h"
+
+Parser::Parser(Scanner &scn) : scanner_(scn) {}
+Parser::~Parser() {}
+void Parser::Parse()
+{
+}
+double Parser::Calcuate() const
+{
+    return 0.0;
+}
