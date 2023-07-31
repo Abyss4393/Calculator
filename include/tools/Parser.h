4e8c76039b8f9fd8b4eedcb28d60927c58d36a9e
wql <2294336102@qq.com>
2023-07-31 08:27:50 +0800

    解析器头文件

--- include/tools/Parser.h
+++ include/tools/Parser.h
@@ -1,5 +1,16 @@
+
 #ifndef _PARSER_H_
 #define _PARSER_H_
+class Scanner;
 
+class Parser
+{
+private:
+    Scanner& scanner_;
+
+public:
+    Parser(Scanner &scn) : scanner_(scn){};
+    ~Parser(){};
+};
 
 #endif // _PARSER_H_
