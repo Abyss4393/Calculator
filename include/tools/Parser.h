7362dacadf6bdc886054bde9b6b943a37fd81ee5
wql <2294336102@qq.com>
2023-08-01 14:28:06 +0800

    完善部分功能

--- include/tools/Parser.h
+++ include/tools/Parser.h
@@ -1,17 +1,30 @@
-
 #ifndef _PARSER_H_
 #define _PARSER_H_
+
+class Node;
 class Scanner;
+
+enum Status
+{
+    OK,
+    QUIT,
+    S_ERROR
+};
 
 class Parser
 {
 private:
     Scanner &scanner_;
+    Node *tree_;
+    Status status_;
 
 public:
     Parser(Scanner &scn);
     ~Parser();
     void Parse();
+    Node *Expr();
+    Node *Term();
+    Node *Factor();
     double Calcuate() const;
 };
 
